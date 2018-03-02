58d1857d911e1e35e141e41a9755d43cc9b62489
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-02 10:15:52 +0300

    post and get controllers are added for login

--- src/main/java/com/trendyol/springbootpractice/controller/LoginController.java
+++ src/main/java/com/trendyol/springbootpractice/controller/LoginController.java
@@ -3,18 +3,27 @@ package com.trendyol.springbootpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    //@ResponseBody
+    public String showLoginMessage(ModelMap model){
+        //model.put("name", name);
+        //System.out.println(name);
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     //@ResponseBody
     public String loginMessage(@RequestParam String name, ModelMap model){
         model.put("name", name);
-        System.out.println(name);
-        return "login";
+        //System.out.println(name);
+        return "welcome";
     }
 
 }
