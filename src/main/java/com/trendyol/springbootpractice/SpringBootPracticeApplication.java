0552aeb952266e953c7f503aeedebca90502ce8e
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-02 09:24:00 +0300

    Initial Commit

---
+++ src/main/java/com/trendyol/springbootpractice/SpringBootPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.trendyol.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = { "com.trendyol"} )
+public class SpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/trendyol/springbootpractice/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.trendyol.springbootpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    //@ResponseBody
+    public String loginMessage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
+        System.out.println(name);
+        return "login";
+    }
+
+}
