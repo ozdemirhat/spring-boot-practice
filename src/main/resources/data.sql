38d4c47825422cb1f4a212badb1422b6b702afdd
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-08 13:34:29 +0300

    initial todo sql is written

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into TODO
+values (1001, 'Learn Spring Boot', false, sysdate(), 'hatice');
+
+insert into TODO
+values (1002, 'Learn Woodworking', false, sysdate(), 'hatice');
+
+insert into TODO
+values (1003, 'Learn how to stand someone you hate', false, sysdate(), 'hatice');
